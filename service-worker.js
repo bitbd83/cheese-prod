59d4471800a19091baa6275b014531b159372d69
bitbd83 <bitbd83@gmail.com>
2025-11-29 01:48:11 +0600

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d2e1ec16114367ce2a8332c91ae2cb9c.js"
+  "./precache-manifest.1147b78441a538409460be2375a98f8d.js"
 );
 
 self.addEventListener('message', (event) => {
